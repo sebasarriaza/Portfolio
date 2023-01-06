426dba6e761fd8d9d978b6da8f5369244f2422ce
sebas <sebas28ac@gmail.com>
2023-01-06 21:25:16 +0100

    Restyling website, adding font-family

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,46 +1,60 @@
-//Header animation
-const $ = name => document.querySelector('.' + name).classList.add(name + '-animation');
-const $2 = name => document.querySelector('.' + name).classList.remove(name + '-animation');
+//Add animation
+const $ = (name) =>
+  document.querySelector("." + name).classList.add(name + "-animation");
+const $2 = (name) =>
+  document.querySelector("." + name).classList.remove(name + "-animation");
 
 function headerAnimation() {
-    if (document.documentElement.scrollTop != 0) {
-        let nav = $('nav');
-        let nav__home = $('nav__home');
-        let nav__img = $('nav__img');
-        let nav__menu = $('nav__menu');
-        let nav__projects = $('nav__projects');
-        let nav__about = $('nav__about');
-        let nav__contact = $('nav__contact');
-    } else{
-        let nav = $2('nav');
-        let nav__home = $2('nav__home');
-        let nav__img = $2('nav__img');
-        let nav__menu = $2('nav__menu');
-        let nav__projects = $2('nav__projects');
-        let nav__about = $2('nav__about');
-        let nav__contact = $2('nav__contact');
-    }
-};
+  if (document.documentElement.scrollTop != 0) {
+    let nav = $("nav");
+    let nav__home = $("nav__home");
+    let nav__name = $("nav__name");
+    let nav__menu = $("nav__menu");
+    let nav__projects = $("nav__projects");
+    let nav__about = $("nav__about");
+    let nav__contact = $("nav__contact");
+    // let presentation__span = $('presentation__span');
+  } else {
+    let nav = $2("nav");
+    let nav__home = $2("nav__home");
+    let nav__name = $2("nav__name");
+    let nav__menu = $2("nav__menu");
+    let nav__projects = $2("nav__projects");
+    let nav__about = $2("nav__about");
+    let nav__contact = $2("nav__contact");
+    // let presentation__span = $2("presentation__span");
+  }
+}
 
-window.addEventListener('scroll', headerAnimation);
+// function hideName() {
+//   if (document.documentElement.scrollTop < 136) {
+//     document.getElementById("nav__span").innerHTML = "";
+//     document.getElementById("presentation__span").innerHTML = "EBASARRIAZA";
+// } else {
+//     document.getElementById("nav__span").innerHTML = "EBASARRIAZA";
+//     document.getElementById("presentation__span").innerHTML = "";
+//   }
+// }
 
+window.addEventListener("scroll", headerAnimation);
+// window.addEventListener("scroll", hideName);
 
 // Contact form
-const form = document.querySelector('#form')
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(event){
-    event.preventDefault();
-    const formData = new FormData(form);
-    const response = await fetch(this.action, {
-        method: this.method,
-        body: formData,
-        headers: {
-            'Accept': 'application/json'
-        }
-    });
-    if(response.ok){
-        form.reset();
-    }
+async function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    form.reset();
+  }
 }
