4936d9db698e1aab01ca8a2564232a861c17ca19
sebas <sebas28ac@gmail.com>
2023-01-14 20:26:20 +0100

    Zoom video and sticky presentation

--- scripts/main.js
+++ scripts/main.js
@@ -8,20 +8,20 @@ function headerAnimation() {
   if (document.documentElement.scrollTop != 0) {
     let nav = $("nav");
     let nav__home = $("nav__home");
-    let nav__name = $("nav__name");
     let nav__menu = $("nav__menu");
     let nav__projects = $("nav__projects");
     let nav__about = $("nav__about");
     let nav__contact = $("nav__contact");
+    let background__video = $("background__video");
     // let presentation__span = $('presentation__span');
   } else {
     let nav = $2("nav");
     let nav__home = $2("nav__home");
-    let nav__name = $2("nav__name");
     let nav__menu = $2("nav__menu");
     let nav__projects = $2("nav__projects");
     let nav__about = $2("nav__about");
     let nav__contact = $2("nav__contact");
+    let background__video = $2("background__video");
     // let presentation__span = $2("presentation__span");
   }
 }
