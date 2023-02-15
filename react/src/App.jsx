3cbc4cf380003649991a581ece2d4f256ef9c6fe
sebas <sebas28ac@gmail.com>
2023-02-15 01:11:38 +0100

    Create Nav component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Nav />
     </div>
   )
 }
