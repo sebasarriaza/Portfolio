f0f712f44fff520af4c3f0b8f6f26255b492f4e1
sebas <sebas28ac@gmail.com>
2023-02-17 03:47:44 +0100

    Modify SocialMedia props (change children to icon)

--- react/src/components/AboutSection.jsx
+++ react/src/components/AboutSection.jsx
@@ -1,7 +1,6 @@
 import SectionTemplate from "./SectionTemplate";
 import SocialMedia from "./SocialMedia";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function AboutSection() {
   return (
@@ -10,9 +9,7 @@ function AboutSection() {
         title="About"
         description="Hi, my name is Sebas. I am a frontend web developer with 2 years of experience developing websites. Skills: React, JavaScript, CSS, HTML, Tailwind, GIT, Bash, Linux, Figma, Photoshop and Lightroom."
       >
-        <SocialMedia userName="sebasarriaza">
-          <FontAwesomeIcon icon={faInstagram} />
-        </SocialMedia>
+        <SocialMedia icon={faGithub} userName="sebasarriaza" />
       </SectionTemplate>
     </section>
   );
--- react/src/components/SocialMedia.jsx
+++ react/src/components/SocialMedia.jsx
@@ -1,7 +1,9 @@
-function SocialMedia({ children, userName }) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function SocialMedia({ icon, userName }) {
   return (
     <div>
-      {children}
+      <FontAwesomeIcon icon={icon} size='10x' />
       <h3>{userName}</h3>
     </div>
   );
