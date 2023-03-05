fa7c7d8e27b95b78317355f08c16ba104be8ac33
sebas <sebas28ac@gmail.com>
2023-03-05 19:56:49 +0100

    Update SocialMedia links

--- react/src/components/SocialMedia.jsx
+++ react/src/components/SocialMedia.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function SocialMedia({ icon, userName, link=`https://${icon.iconName}.com/${userName}` }) {
   return (
     <div>
-      <a href={link}>
+      <a href={link} target='_blank'>
       <FontAwesomeIcon icon={icon} size='10x' />
       <h3>{userName}</h3>
       </a>
