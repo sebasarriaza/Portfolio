ebca2d688c047d098304ccd48bc6f9f76f071f2f
sebas <sebas28ac@gmail.com>
2023-06-04 11:34:31 +0200

    Add react-scroll

--- nextjs/src/components/Navbar.tsx
+++ nextjs/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
-
 import React, { useState } from "react";
+import { Link, Element } from "react-scroll";
 
 const Navbar: React.FC = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -9,14 +9,9 @@ const Navbar: React.FC = () => {
     setOpenMenu(event.target.checked);
   };
 
-  const navigateToSection = (section:string) => {
-    document?.getElementById(section)?.scrollIntoView({ behavior: 'smooth'});
-  }
-  
-
   return (
-    <nav className="fixed flex justify-between w-[100%] menu-container z-20">
-      <div className="flex p-5">
+    <nav className="fixed flex justify-between w-[100%] z-20">
+      <div className="flex p-5 h-fit">
         <a href="#">
           <h3>Sebas Arriaza</h3>
         </a>
@@ -32,45 +27,72 @@ const Navbar: React.FC = () => {
 
       <div className="p-5">
         <div className="flex justify-end">
-            <label htmlFor="openmenu">
+          <label htmlFor="openmenu">
             <span
-                className={`bg-white h-[0.1rem] w-7 block my-[0.4rem] transition duration-700 ease-in-out
+              className={`bg-white h-[0.1rem] w-7 block my-[0.4rem] transition duration-700 ease-in-out
                 ${
-                openMenu ? "translate-y-[500%] rotate-[-45deg]" : "transform-none"
+                  openMenu
+                    ? "translate-y-[500%] rotate-[-45deg]"
+                    : "transform-none"
                 }`}
             ></span>
             <span
-                className={`bg-white h-[0.1rem] w-7 block my-[0.4rem] transition duration-700 ease-in-out
+              className={`bg-white h-[0.1rem] w-7 block my-[0.4rem] transition duration-700 ease-in-out
                 ${openMenu ? "rotate-[45deg]" : "transform-none"}`}
             ></span>
-            </label>
+          </label>
         </div>
-      <div className={`
+        <div
+          className={`
         transition duration-700 ease-in-out mt-10
         ${openMenu ? "translate-x-[0]" : "translate-x-[50vw]"}
-        `}>
-        <div className=" ">
+        `}
+        >
+          <div className=" ">
             <ul className="flex flex-col items-end w-[100%] justify-end gap-4 ">
-                <li className="flex">
-                <button className="flex font-thin" onClick={() => navigateToSection("projects")}>
+              <li className="flex">
+                <Link
+                  className="flex font-thin"
+                  activeClass="active"
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
                   Projects<span className="text-xs">01</span>
-                </button>
-                </li>
-                <li>
-                    <button className="flex" onClick={() => navigateToSection("about")}>
-                    About<span className="text-xs">02</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="flex" onClick={() => navigateToSection("contact")}>
-                    Contact<span className="text-xs">03</span>
-                    </button>
-                </li>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex"
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  About<span className="text-xs">02</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex"
+                  activeClass="active"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Contact<span className="text-xs">03</span>
+                </Link>
+              </li>
             </ul>
+          </div>
         </div>
       </div>
-      </div>
-
     </nav>
   );
 };
