0d34105c5515919c5dd8166432902bb964af48dd
sebas <sebas28ac@gmail.com>
2023-06-07 03:50:46 +0200

    Style About Section

--- nextjs/src/components/sections/AboutSection.tsx
+++ nextjs/src/components/sections/AboutSection.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import solidity from "@/public/images/solidity.svg";
 import hardhat from "@/public/images/hardhat.svg";
 import ethers from "@/public/images/ethers.svg";
-import chainlink from "@/public/images/chainlink.svg";
+import wagmi from "@/public/images/wagmi.svg";
+import rainbowkit from "@/public/images/rainbowkit.svg";
 import alchemy from "@/public/images/alchemy.svg";
+import chainlink from "@/public/images/chainlink.svg";
 import ethereum from "@/public/images/ethereum.svg";
 import polygon from "@/public/images/polygon.svg";
 import javascript from "@/public/images/javascript.svg";
@@ -28,52 +30,73 @@ import turborepo from "@/public/images/turborepo.svg";
 import bash from "@/public/images/bash.svg";
 import kali from "@/public/images/kali.svg";
 
-
 function AboutSection() {
   return (
     <SectionTemplate title="About">
-      <section>
-        <p>I am a fullstack web3 developer. In recent years, I have focused on building a solid programming base and creating interesting projects. My experience lies in the development of innovative and visually appealing applications that provide effective solutions. These are my skills:.</p>
-        <p>Web3 technologies: Solidity, Hardhat, Ethers, Wagmi, Rainbowkit, Chainlink oracles, ERC20, ERC721, ERC1155</p>
-        <div className="flex">
-          <Image src={solidity} alt="" height={30} width={30} />
-          <Image src={hardhat} alt="" height={30} width={30} />
-          <Image src={ethers} alt="" height={30} width={30} />
-          <Image src={chainlink} alt="" height={30} width={30} />
-          <Image src={alchemy} alt="" height={30} width={30} />
-          <Image src={ethereum} alt="" height={30} width={30} />
-          <Image src={polygon} alt="" height={30} width={30} />
+      <div>
+        <p className="tracking-wider lg:text-lg">
+          I am a fullstack web3 developer. In recent years, I have focused on
+          building a solid programming base and creating interesting projects.
+          My experience lies in the development of innovative and visually
+          appealing applications that provide effective solutions. These are my
+          skills:
+        </p>
+        <div className="flex flex-col lg:flex-row lg:justify-between">
+          <div className="flex flex-col justify-center">
+            <p>
+              Web3 technologies:
+            </p>
+            <div className="grid grid-cols-5 gap-4 lg:grid-cols-3">
+                <Image src={solidity} alt="" height={30} width={30} />
+                <Image src={hardhat} alt="" height={30} width={30} />
+                <Image src={ethers} alt="" height={30} width={30} />
+                <Image src={wagmi} alt="" height={30} width={30} />
+                <Image src={rainbowkit} alt="" height={30} width={30} />
+                <Image src={chainlink} alt="" height={30} width={30} />
+                <Image src={alchemy} alt="" height={30} width={30} />
+                <Image src={ethereum} alt="" height={30} width={30} />
+                <Image src={polygon} alt="" height={30} width={30} />
+            </div>
+          </div>
+          <div>
+            <p>Web2 technologies:</p>
+            <div className="grid grid-cols-5 gap-4 lg:grid-cols-3">
+                <Image src={javascript} alt="" height={30} width={30} />
+                <Image src={typescript} alt="" height={30} width={30} />
+                <Image src={react} alt="" height={30} width={30} />
+                <Image src={vite} alt="" height={30} width={30} />
+                <Image src={nextjs} alt="" height={30} width={30} />
+                <Image src={prisma} alt="" height={30} width={30} />
+                <Image src={t3} alt="" height={30} width={30} />
+            </div>
+          </div>
+          <div>
+            <p>
+              UI/UX technologies:
+            </p>
+            <div className="grid grid-cols-5 gap-4 lg:grid-cols-3">
+                <Image src={html} alt="" height={30} width={30} />
+                <Image src={css} alt="" height={30} width={30} />
+                <Image src={sass} alt="" height={30} width={30} />
+                <Image src={tailwind} alt="" height={30} width={30} />
+                <Image src={materialui} alt="" height={30} width={30} />
+                <Image src={chakra} alt="" height={30} width={30} />
+                <Image src={figma} alt="" height={30} width={30} />
+                <Image src={adobexd} alt="" height={30} width={30} />
+                <Image src={photoshop} alt="" height={30} width={30} />
+            </div>
+          </div>
+          <div>
+            <p>Other technologies:</p>
+            <div className="grid grid-cols-5 gap-4 lg:grid-cols-3">
+                <Image src={git} alt="" height={30} width={30} />
+                <Image src={turborepo} alt="" height={30} width={30} />
+                <Image src={bash} alt="" height={30} width={30} />
+                <Image src={kali} alt="" height={30} width={30} />
+            </div>
+          </div>
         </div>
-        <p>Web2 technologies: Javascript, Typescrypt, React, Vite, Nextjs, Prisma, T3</p>
-        <div className="flex">
-          <Image src={javascript} alt="" height={30} width={30} />
-          <Image src={typescript} alt="" height={30} width={30} />
-          <Image src={react} alt="" height={30} width={30} />
-          <Image src={vite} alt="" height={30} width={30} />
-          <Image src={nextjs} alt="" height={30} width={30} />
-          <Image src={prisma} alt="" height={30} width={30} />
-          <Image src={t3} alt="" height={30} width={30} />
-        </div>
-        <p>UI/UX technologies: HTML, CSS, Sass, Tailwind, Material UI, Figma, Adobexd, Lightroom, Photoshop</p>
-        <div className="flex">
-          <Image src={html} alt="" height={30} width={30} />
-          <Image src={css} alt="" height={30} width={30} />
-          <Image src={sass} alt="" height={30} width={30} />
-          <Image src={tailwind} alt="" height={30} width={30} />
-          <Image src={materialui} alt="" height={30} width={30} />
-          <Image src={chakra} alt="" height={30} width={30} />
-          <Image src={figma} alt="" height={30} width={30} />
-          <Image src={adobexd} alt="" height={30} width={30} />
-          <Image src={photoshop} alt="" height={30} width={30} />
-        </div>
-        <p>Other technologies: GIT, Turborepo Bash, Linux</p>
-        <div className="flex">
-          <Image src={git} alt="" height={30} width={30} />
-          <Image src={turborepo} alt="" height={30} width={30} />
-          <Image src={bash} alt="" height={30} width={30} />
-          <Image src={kali} alt="" height={30} width={30} />
-        </div>
-      </section>
+      </div>
     </SectionTemplate>
   );
 }
--- nextjs/src/components/sections/SectionTemplate.tsx
+++ nextjs/src/components/sections/SectionTemplate.tsx
@@ -16,7 +16,7 @@ function SectionTemplate({
       <div className="flex text-5xl px-5 pb-[5vh] lg:pb-[0vh] lg:px-[4.4rem]">
         <h2>{titleOrCustomTitle}</h2>
       </div>
-      <div className="relative max-h-fit min-h-fit mb-px p-8 border rounded-2xl bg-zinc-950 overflow-hidden lg:m-[4.4rem]">
+      <div className="relative mb-px p-8 border rounded-2xl bg-zinc-950 overflow-hidden lg:m-[4.4rem]">
         {children}
       </div>
     </section>
