887c3725ca8a25070ad1f2c96ce2d4a30bbba7d1
sebas <sebas28ac@gmail.com>
2023-06-10 20:53:01 +0200

    Show alert if you submit an empty form

--- nextjs/src/components/sections/ContactSection.tsx
+++ nextjs/src/components/sections/ContactSection.tsx
@@ -1,24 +1,36 @@
+'use client'
+import React from 'react';
 import SectionTemplate from './SectionTemplate';
 
 function ContactSection() {
+  const validateForm = (event: React.FormEvent<HTMLFormElement>) => {
+    const nameInput = (document.getElementById('nameInput') as HTMLInputElement).value;
+    const emailInput = (document.getElementById('emailInput') as HTMLInputElement).value;
+    const messageInput = (document.getElementById('messageInput') as HTMLInputElement).value;
+
+    if (nameInput === '' || emailInput === '' || messageInput === '') {
+      alert('Por favor, completa todos los campos');
+      event.preventDefault(); // Evita el envío del formulario si los campos están vacíos
+    }
+  };
+
   return (
     <SectionTemplate title="Contact">
-        <p className="mb-3 tracking-wider lg:text-xl lg:p-4">Feel free to reach out to me for any inquiries, collaborations, or projects where I can be of assistance!</p>
-        <form className='text-black' action="https://formspree.io/f/myyvgpoe" method="POST">
-            <div className='flex flex-col lg:flex-row lg:justify-between'>
-              <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:mr-3' type='text' placeholder='NAME'/>
-              <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:ml-3' type='email' placeholder='EMAIL'/>
-            </div>
-            <div className='flex flex-col'>
-            <textarea
-              className='my-3 p-3 h-36 rounded-md resize-none'
-              placeholder='MESSAGE'
-            />
-            </div>
-            <div className='bg-zinc-300 mt-3 rounded-lg'>
-              <input className="p-3 w-full cursor-pointer" type="submit" value="Send"/>
-            </div>
-        </form>
+      <p className="mb-3 tracking-wider lg:text-xl lg:p-4">
+        Feel free to reach out to me for any inquiries, collaborations, or projects where I can be of assistance!
+      </p>
+      <form className='text-black' action="https://formspree.io/f/myyvgpoe" method="POST" onSubmit={validateForm}>
+        <div className='flex flex-col lg:flex-row lg:justify-between'>
+          <input id="nameInput" className='my-3 p-3 h-12 rounded-md lg:w-full lg:mr-3' type='text' placeholder='NAME' />
+          <input id="emailInput" className='my-3 p-3 h-12 rounded-md lg:w-full lg:ml-3' type='email' placeholder='EMAIL' />
+        </div>
+        <div className='flex flex-col'>
+          <textarea id="messageInput" className='my-3 p-3 h-36 rounded-md resize-none' placeholder='MESSAGE'></textarea>
+        </div>
+        <div className='bg-zinc-300 mt-3 rounded-lg'>
+          <input className="p-3 w-full cursor-pointer" type="submit" value="Send" />
+        </div>
+      </form>
     </SectionTemplate>
   );
 }
