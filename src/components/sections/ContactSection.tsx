4dc6452d5a02545f93f8dec9dcb30f59d5313f83
sebas <sebas28ac@gmail.com>
2023-06-11 20:02:21 +0200

    Add validation to require filling in all fields in ContactSection

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { FormEvent, useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import SectionTemplate from './SectionTemplate';
 import { useForm, ValidationError } from '@formspree/react';
 
@@ -9,6 +9,21 @@ function ContactSection() {
 
   const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const elements = event.currentTarget.elements as HTMLFormControlsCollection;
+    const nameInput = elements.namedItem("name") as HTMLInputElement;
+    const emailInput = elements.namedItem("email") as HTMLInputElement;
+    const messageInput = elements.namedItem("message") as HTMLTextAreaElement;
+
+    if (!nameInput || !emailInput || !messageInput) {
+      return;
+    }
+
+    if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
+      alert('Please fill in all fields.');
+      return;
+    }
+
     await handleSubmit(event);
     setIsFormSubmitted(true);
   };
@@ -33,17 +48,17 @@ function ContactSection() {
       </p>
       <form className='text-black' onSubmit={handleFormSubmit}>
         <div className='flex flex-col lg:flex-row lg:justify-between'>
-          <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:mr-3' placeholder='NAME' id="name" type='text' name="name"/>
-          <ValidationError prefix="Name" field="name" errors={state.errors}/>
+          <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:mr-3' placeholder='NAME' id="name" type='text' name="name" />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
           <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:ml-3' placeholder='EMAIL' id="email" type='email' name="email" />
-          <ValidationError prefix="Email" field="email" errors={state.errors}/>
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <div className='flex flex-col'>
-          <textarea className='my-3 p-3 h-36 rounded-md resize-none' placeholder='MESSAGE' id="message" name="message"/>
-          <ValidationError prefix="Message" field="message" errors={state.errors}/>
+          <textarea className='my-3 p-3 h-36 rounded-md resize-none' placeholder='MESSAGE' id="message" name="message" />
+          <ValidationError prefix="Message" field="message" errors={state.errors} />
         </div>
         <div className='bg-zinc-300 mt-3 rounded-lg'>
-          <input className="p-3 w-full cursor-pointer" type="submit" value="Submit"/>
+          <input className="p-3 w-full cursor-pointer" type="submit" value="Submit" />
         </div>
       </form>
       {isFormSubmitted && (
