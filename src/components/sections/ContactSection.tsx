d5f212261a896935dd7ed6aa0098659c2705e904
sebas <sebas28ac@gmail.com>
2023-06-11 19:46:09 +0200

    Fix contact-form

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,36 +1,54 @@
 'use client'
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import SectionTemplate from './SectionTemplate';
+import { useForm, ValidationError } from '@formspree/react';
 
 function ContactSection() {
-  const validateForm = (event: React.FormEvent<HTMLFormElement>) => {
-    const nameInput = (document.getElementById('nameInput') as HTMLInputElement).value;
-    const emailInput = (document.getElementById('emailInput') as HTMLInputElement).value;
-    const messageInput = (document.getElementById('messageInput') as HTMLInputElement).value;
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [state, handleSubmit, resetForm] = useForm("xlekyqzg");
 
-    if (nameInput === '' || emailInput === '' || messageInput === '') {
-      alert('Please fill in all the fields');
-      event.preventDefault();
-    }
+  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await handleSubmit(event);
+    setIsFormSubmitted(true);
   };
+
+  const handleResetForm = () => {
+    resetForm();
+    setIsFormSubmitted(false);
+  };
+
+  if (state.succeeded && isFormSubmitted) {
+    return (
+      <SectionTemplate title="Contact">
+        <p>Thanks for joining!</p>
+      </SectionTemplate>
+    );
+  }
 
   return (
     <SectionTemplate title="Contact">
       <p className="mb-3 tracking-wider lg:text-xl lg:p-4">
         Feel free to reach out to me for any inquiries, collaborations, or projects where I can be of assistance!
       </p>
-      <form className='text-black' action="https://formspree.io/f/myyvgpoe" method="POST" onSubmit={validateForm}>
+      <form className='text-black' onSubmit={handleFormSubmit}>
         <div className='flex flex-col lg:flex-row lg:justify-between'>
-          <input id="nameInput" className='my-3 p-3 h-12 rounded-md lg:w-full lg:mr-3' type='text' placeholder='NAME' />
-          <input id="emailInput" className='my-3 p-3 h-12 rounded-md lg:w-full lg:ml-3' type='email' placeholder='EMAIL' />
+          <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:mr-3' placeholder='NAME' id="name" type='text' name="name"/>
+          <ValidationError prefix="Name" field="name" errors={state.errors}/>
+          <input className='my-3 p-3 h-12 rounded-md lg:w-full lg:ml-3' placeholder='EMAIL' id="email" type='email' name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors}/>
         </div>
         <div className='flex flex-col'>
-          <textarea id="messageInput" className='my-3 p-3 h-36 rounded-md resize-none' placeholder='MESSAGE'></textarea>
+          <textarea className='my-3 p-3 h-36 rounded-md resize-none' placeholder='MESSAGE' id="message" name="message"/>
+          <ValidationError prefix="Message" field="message" errors={state.errors}/>
         </div>
         <div className='bg-zinc-300 mt-3 rounded-lg'>
-          <input className="p-3 w-full cursor-pointer" type="submit" value="Send" />
+          <input className="p-3 w-full cursor-pointer" type="submit" value="Submit"/>
         </div>
       </form>
+      {isFormSubmitted && (
+        <button onClick={handleResetForm}>Reset Form</button>
+      )}
     </SectionTemplate>
   );
 }
