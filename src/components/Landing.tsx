ecc85ec283ef19da3de501502d5633454f2a2490
Sebas Arriaza <97684343+sebasarriaza@users.noreply.github.com>
2024-03-17 21:51:13 +0000

    Delete X from Social Media and reorder Links

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,16 +5,16 @@ import {faInstagram, faXTwitter, faGithub, faLinkedinIn} from "@fortawesome/free
 function Landing() {
   return (
     <div className="sticky top-0 z-0">
-      <div className="absolute flex flex-col mt-[45vh] xl:mt-[55vh]">
-        <SocialMedia icon={faLinkedinIn} userName="sebasarriaza" link="https://www.linkedin.com/in/sebasarriaza/" />
-        <SocialMedia icon={faGithub} userName="sebasarriaza" />
-        <SocialMedia icon={faXTwitter} userName="sebasarriaza" link="https://twitter.com/sebasarriaza"/>
-        <SocialMedia icon={faInstagram} userName="sebasarriaza" />
+      <div className="absolute flex flex-col mt-[50vh] xl:mt-[55vh]">
         <SocialMedia
           icon={faEnvelope}
           userName="contact@sebasarriaza.com"
           link="mailto:contact@sebasarriaza.com"
         />
+        <SocialMedia icon={faLinkedinIn} userName="sebasarriaza" link="https://www.linkedin.com/in/sebasarriaza/" />
+        <SocialMedia icon={faGithub} userName="sebasarriaza" />
+        {/* <SocialMedia icon={faXTwitter} userName="sebasarriaza" link="https://twitter.com/sebasarriaza"/> */}
+        <SocialMedia icon={faInstagram} userName="sebasarriaza" />
       </div>
       <div className="flex flex-col items-center justify-center pt-[30vh] pb-[60vh]">
         <h1 className="text-4xl font-bold sm:text-6xl lg:text-[4rem] xl:text-[6rem] 2xl:text-[6.5rem]">
